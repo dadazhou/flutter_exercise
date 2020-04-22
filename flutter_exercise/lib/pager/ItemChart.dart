c27226f5fb2e93b5da492bf181133025a0684fb0
dadazhou <271677247@qq.com>
2020-04-22 13:08:57 +0800

    添加itemChart仿微信条目栏

--- flutter_exercise/lib/pager/ItemChart.dart
+++ flutter_exercise/lib/pager/ItemChart.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ItemChart extends StatefulWidget{
+class ItemChart extends StatefulWidget {
+  final ChartBean chartBean;
+
+  ItemChart({Key key, this.chartBean}) : super(key: key);
 
   @override
-  _ItemChartState createState() {
-
-  }
+  _ItemChartState createState() => _ItemChartState();
 }
 
-class _ItemChartState extends State<ItemChart>{
-
+class _ItemChartState extends State<ItemChart> {
+  bool _checked;//是否已查看
+  ChartBean _chartBean;//条目描述类
   @override
   Widget build(BuildContext context) {
 
+    var left = Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: widget.chartBean.image,
+      )
+    );
+
+    var body = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[Padding(padding: EdgeInsets.only(right: 10),child: _checked?left:leftWrap,), Expanded(child: center,), right],
+    );
+    return Container(
+      child: body,
+    );
   }
+}
+
+class ChartBean {
+  Image image; //头像
+  String name; //名字
+  String sentence; //句子
+  String time; //时间
+  bool shield; //是否屏蔽
+
+  ChartBean(
+      {this.image, this.name, this.sentence, this.time, this.shield = false});
 }
